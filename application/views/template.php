b9d7ebbcea7b6da813982025ddd1166d0a620cc0
David Lawson <tech.lawson@gmail.com>
2013-10-30 05:35:23 +0000

    fixing template rewriting bugs

--- application/views/template.php
+++ application/views/template.php
@@ -1,4 +1,26 @@
-<!DOCTYPE html>
+<?php
+
+function generate_breadcrumbs($breadcrumbs)
+{
+    $output = '';
+    if (count($breadcrumbs) > 0)
+    {
+        $output .= '<ul class="breadcrumb">';
+        foreach ($breadcrumbs as $breadcrumb)
+        {
+            $output .=  '<li>';
+            if ($breadcrumb[1] && $breadcrumb != end($breadcrumbs)) $output .= '<a href="'.URL::site($breadcrumb[1]).'">';
+            $output .= $breadcrumb[0];
+            if ($breadcrumb[1] && $breadcrumb != end($breadcrumbs)) $output .= '</a>';
+            $output .= '</li>';
+        }
+        $output .= '</ul>';
+    }
+
+    return $output;
+}
+
+?><!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
@@ -27,22 +49,24 @@
         <!-- Place everything within .navbar-collapse to hide it until above 768px -->
         <div class="nav-collapse collapse navbar-responsive-collapse">
             <ul class="nav navbar-nav">
-                <li <?php if(Request::$current->controller() == 'Users'): ?>class="active"<?php endif ?>><a href="<?php echo URL::site('participants')?>">Participants</a></li>
-                <li <?php if(Request::$current->controller() == 'Images'): ?>class="active"<?php endif ?>><a href="<?php echo URL::site('images') ?>">Images</a></li>
+                <li <?php if(Request::$current && Request::$current->controller() == 'Users'): ?>class="active"<?php endif ?>><a href="<?php echo URL::site('participants')?>">Participants</a></li>
+                <li <?php if(Request::$current && Request::$current->controller() == 'Images'): ?>class="active"<?php endif ?>><a href="<?php echo URL::site('images') ?>">Images</a></li>
                 <li><a href="http://www.psypad.net.au/" target="_blank">Wiki</a></li>
                 <li><a href="https://github.com/davidlawson/PsyPad" target="_blank">App Repository</a></li>
                 <li><a href="https://github.com/davidlawson/PsyPad-Server" target="_blank">Server Repository</a></li>
             </ul>
-            <?php if (Auth::instance()->logged_in()): ?>
-                <p class="navbar-text pull-right">
-                    Logged in as <?php echo Auth::instance()->get_user()->username ?> &nbsp; &nbsp; <a href="<?php echo URL::site('security/logout') ?>" class="navbar-link">Log out</a>
-                </p>
-            <?php else: ?>
-                <form class="navbar-form pull-right" action="<?php echo URL::site('security/login') ?>" method="post">
-                    <input class="form-control" style="width: 200px" type="text" placeholder="Username" name="username">
-                    <input class="form-control" style="width: 200px" type="password" placeholder="Password" name="password">
-                    <button type="submit" class="btn btn-default">Log in</button>
-                </form>
+            <?php if (class_exists('Auth')): ?>
+                <?php if (Auth::instance()->logged_in()): ?>
+                    <p class="navbar-text pull-right">
+                        Logged in as <?php echo Auth::instance()->get_user()->username ?> &nbsp; &nbsp; <a href="<?php echo URL::site('security/logout') ?>" class="navbar-link">Log out</a>
+                    </p>
+                <?php else: ?>
+                    <form class="navbar-form pull-right" action="<?php echo URL::site('security/login') ?>" method="post">
+                        <input class="form-control" style="width: 200px" type="text" placeholder="Username" name="username">
+                        <input class="form-control" style="width: 200px" type="password" placeholder="Password" name="password">
+                        <button type="submit" class="btn btn-default">Log in</button>
+                    </form>
+                <?php endif; ?>
             <?php endif; ?>
         </div><!-- /.nav-collapse -->
     </div>
@@ -57,26 +81,12 @@
             </div><!--/span-->
 
             <div class="col-lg-9">
-                <?php
-                if (count($breadcrumbs) > 0)
-                {
-                    echo '<ul class="breadcrumb">';
-                    foreach ($breadcrumbs as $breadcrumb)
-                    {
-                        echo  '<li>';
-                        if ($breadcrumb[1] && $breadcrumb != end($breadcrumbs)) $output .= '<a href="'.URL::site($breadcrumb[1]).'">';
-                        echo $breadcrumb[0];
-                        if ($breadcrumb[1] && $breadcrumb != end($breadcrumbs)) $output .= '</a>';
-                        echo '</li>';
-                    }
-                    echo '</ul>';
-                }
-                ?>
+                <?php echo generate_breadcrumbs($breadcrumbs); ?>
                 <?php echo $content ?>
             </div>
         <?php else: ?>
             <div class="col-lg-10">
-                <?php echo $breadcrumbs ?>
+                <?php echo generate_breadcrumbs($breadcrumbs) ?>
                 <?php echo $content ?>
             </div>
         <?php endif ?>
