b75c9e22e345c52de3578d84dc63d2d25f403cdd
David Lawson <tech.lawson@gmail.com>
2014-02-03 22:20:33 +1100

    Error handling

--- application/messages/registration/user.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-return array(
-    'password' => array(
-        'matches' => 'Passwords don\'t match',
-        'not_empty' => 'Please enter your password'
-    ),
-);
