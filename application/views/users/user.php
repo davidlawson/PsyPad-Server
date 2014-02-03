73c8b2dd55d1e145e4967cdffa909d4755fc38e0
David Lawson <tech.lawson@gmail.com>
2014-02-03 21:55:41 +1100

    User control panel beginnings

--- application/views/users/user.php
+++ application/views/users/user.php
@@ -1,1 +1,20 @@
 <h1>User Control Panel</h1>
+
+<h2>Change Password</h2>
+<form action="" method="post" style="margin-top: 20px;">
+
+    <div class="form-group">
+        <input class="form-control" type="text" placeholder="Existing password" name="old" style="width: 400px;">
+    </div>
+
+    <div class="form-group">
+        <input class="form-control" type="text" placeholder="New password" name="new" style="width: 400px;">
+    </div>
+
+    <div class="form-group">
+        <input class="form-control" type="text" placeholder="New password (confirm)" name="confirm" style="width: 400px;">
+    </div>
+
+    <input type="submit" value="Change Password" class="btn btn-default">
+
+</form>
