c1d8112568453192e594c8d677f230ec1fb7bbaf
David Lawson <tech.lawson@gmail.com>
2014-02-03 22:04:58 +1100

    Change password feature

--- application/classes/Controller/Users.php
+++ application/classes/Controller/Users.php
@@ -7,6 +7,30 @@ class Controller_Users extends Controller_Template
         $this->template->title = 'User Control Panel';
         $this->template->content = View::factory('users/user');
         $this->template->content->user = Auth::instance()->get_user();
+
+        if ($this->request->method() == HTTP_Request::POST)
+        {
+            $data = $this->request->post();
+            if (Auth::instance()->check_password($data['old']) === FALSE)
+            {
+                $this->template->content->message = "Existing password entered was incorrect";
+                return;
+            }
+            else
+            {
+                unset($data['old']);
+                
+                try
+                {
+                    Auth::instance()->get_user()->update_user($data);
+                    $this->template->content->message = "Password updated successfully";
+                }
+                catch (ORM_Validation_Exception $exception)
+                {
+                    $this->template->content->message = $exception->getMessage();
+                }
+            }
+        }
     }
 
     public function action_users()
--- application/views/users/user.php
+++ application/views/users/user.php
@@ -1,18 +1,20 @@
 <h1>User Control Panel</h1>
 
-<h2>Change Password</h2>
+<?php if (isset($message)): ?><p class="alert alert-info"><?php echo $message ?></p><?php endif ?>
+
+<h3>Change Password</h3>
 <form action="" method="post" style="margin-top: 20px;">
 
     <div class="form-group">
-        <input class="form-control" type="text" placeholder="Existing password" name="old" style="width: 400px;">
+        <input class="form-control" type="password" placeholder="Existing password" name="old" style="width: 400px;">
     </div>
 
     <div class="form-group">
-        <input class="form-control" type="text" placeholder="New password" name="new" style="width: 400px;">
+        <input class="form-control" type="password" placeholder="New password" name="password" style="width: 400px;">
     </div>
 
     <div class="form-group">
-        <input class="form-control" type="text" placeholder="New password (confirm)" name="confirm" style="width: 400px;">
+        <input class="form-control" type="password" placeholder="New password (confirm)" name="password_confirm" style="width: 400px;">
     </div>
 
     <input type="submit" value="Change Password" class="btn btn-default">
