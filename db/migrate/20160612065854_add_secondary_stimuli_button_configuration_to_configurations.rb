2b1503928003c9a64d579948fe2afa86d7484bba
David Lawson <tech.lawson@gmail.com>
2016-06-12 17:09:04 +1000

    Fixing migration

--- db/migrate/20160612065854_add_secondary_stimuli_button_configuration_to_configurations.rb
+++ db/migrate/20160612065854_add_secondary_stimuli_button_configuration_to_configurations.rb
@@ -1,42 +1,42 @@
 class AddSecondaryStimuliButtonConfigurationToConfigurations < ActiveRecord::Migration
   def change
 
-	add_column :configurations, :integer, :num_secondary_buttons, default: 4
+	add_column :configurations, :num_secondary_buttons, :integer, default: 4
 
-	add_column :configurations, :string, :secondary_button1_text
-	add_column :configurations, :string, :secondary_button2_text
-	add_column :configurations, :string, :secondary_button3_text
-	add_column :configurations, :string, :secondary_button4_text
+	add_column :configurations, :secondary_button1_text, :string
+	add_column :configurations, :secondary_button2_text, :string
+	add_column :configurations, :secondary_button3_text, :string
+	add_column :configurations, :secondary_button4_text, :string
 
-	add_column :configurations, :string, :secondary_button1_bg, default: '#6c6c6c'
-	add_column :configurations, :string, :secondary_button2_bg, default: '#6c6c6c'
-	add_column :configurations, :string, :secondary_button3_bg, default: '#6c6c6c'
-	add_column :configurations, :string, :secondary_button4_bg, default: '#6c6c6c'
+	add_column :configurations, :secondary_button1_bg, :string, default: '#6c6c6c'
+	add_column :configurations, :secondary_button2_bg, :string, default: '#6c6c6c'
+	add_column :configurations, :secondary_button3_bg, :string, default: '#6c6c6c'
+	add_column :configurations, :secondary_button4_bg, :string, default: '#6c6c6c'
 
-	add_column :configurations, :string, :secondary_button1_fg, default: '#ffffff'
-	add_column :configurations, :string, :secondary_button2_fg, default: '#ffffff'
-	add_column :configurations, :string, :secondary_button3_fg, default: '#ffffff'
-	add_column :configurations, :string, :secondary_button4_fg, default: '#ffffff'
+	add_column :configurations, :secondary_button1_fg, :string, default: '#ffffff'
+	add_column :configurations, :secondary_button2_fg, :string, default: '#ffffff'
+	add_column :configurations, :secondary_button3_fg, :string, default: '#ffffff'
+	add_column :configurations, :secondary_button4_fg, :string, default: '#ffffff'
 
-	add_column :configurations, :integer, :secondary_button1_x, default: 237
-	add_column :configurations, :integer, :secondary_button1_y, default: 698
-	add_column :configurations, :integer, :secondary_button1_w, default: 100
-	add_column :configurations, :integer, :secondary_button1_h, default: 40
+	add_column :configurations, :secondary_button1_x, :integer, default: 237
+	add_column :configurations, :secondary_button1_y, :integer, default: 698
+	add_column :configurations, :secondary_button1_w, :integer, default: 100
+	add_column :configurations, :secondary_button1_h, :integer, default: 40
 
-	add_column :configurations, :integer, :secondary_button2_x, default: 387
-	add_column :configurations, :integer, :secondary_button2_y, default: 698
-	add_column :configurations, :integer, :secondary_button2_w, default: 100
-	add_column :configurations, :integer, :secondary_button2_h, default: 40
+	add_column :configurations, :secondary_button2_x, :integer, default: 387
+	add_column :configurations, :secondary_button2_y, :integer, default: 698
+	add_column :configurations, :secondary_button2_w, :integer, default: 100
+	add_column :configurations, :secondary_button2_h, :integer, default: 40
 
-	add_column :configurations, :integer, :secondary_button3_x, default: 537
-	add_column :configurations, :integer, :secondary_button3_y, default: 698
-	add_column :configurations, :integer, :secondary_button3_w, default: 100
-	add_column :configurations, :integer, :secondary_button3_h, default: 40
+	add_column :configurations, :secondary_button3_x, :integer, default: 537
+	add_column :configurations, :secondary_button3_y, :integer, default: 698
+	add_column :configurations, :secondary_button3_w, :integer, default: 100
+	add_column :configurations, :secondary_button3_h, :integer, default: 40
 
-	add_column :configurations, :integer, :secondary_button4_x, default: 687
-	add_column :configurations, :integer, :secondary_button4_y, default: 698
-	add_column :configurations, :integer, :secondary_button4_w, default: 100
-	add_column :configurations, :integer, :secondary_button4_h, default: 40
+	add_column :configurations, :secondary_button4_x, :integer, default: 687
+	add_column :configurations, :secondary_button4_y, :integer, default: 698
+	add_column :configurations, :secondary_button4_w, :integer, default: 100
+	add_column :configurations, :secondary_button4_h, :integer, default: 40
 
   end
 end
